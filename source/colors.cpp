da9c6d70571f8f4bc66a0d9e839fe5e3ca7d4117
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2025-04-07 22:09:50 -0400

    change: colors now works different

---
+++ source/colors.cpp
@@ -0,0 +1,6 @@
+#include "colors.hpp"
+
+
+namespace bf
+{
+}
--- source/fight_ring.cpp
+++ source/fight_ring.cpp
@@ -1,6 +1,6 @@
 #include "fight_ring.hpp"
 #include <nothofagus.h>
-#include <color_pallete.hpp>
+#include <colors.hpp>
 
 
 
@@ -9,32 +9,26 @@ namespace bf
     Nothofagus::Bellota FightRing::getBellota(Nothofagus::TextureId textureId) const
     {
         Nothofagus::Bellota bellota = Nothofagus::Bellota({{75.0f, 75.0f}}, textureId);
-        // bellota.transform().scale() = {0.5f, 1.0f};
+        bellota.transform().scale() = {20.0f, 10.0f};
         return bellota;
     }
 
     Nothofagus::Texture FightRing::getTexture() const
     {
-        Nothofagus::Texture texture({21, 15}, {0.5, 0.5, 0.5, 1.0});
+        Nothofagus::Texture texture({8, 4}, {0.5, 0.5, 0.5, 1.0});
         texture.setPallete(getColorPallete()).setPixels(
             {
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0,
-                0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0,
-                0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0,
-                0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0,
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0,
+                0, 0,
             }
         );
         return texture;
+    }
+
+    static Nothofagus::ColorPallete getColorPallete()
+    {
+        return {
+            Colors::brown,
+        };
     }
 }
